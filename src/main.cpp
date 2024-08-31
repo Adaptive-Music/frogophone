6f594c8027c74fa26c4b0fbf1b53a3d43684259c
Vincent Ekpanyaskun <vincent.ekp@gmail.com>
2024-08-31 23:56:45 +1000

    Add major/minor toggle, refactor code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
 #include <MIDI.h>
 
-// Button locations
-// ----------------------
-// Yellow  Purple  Red
-// Orange  Blue    Green
+/*
+  Button locations
+  7. LeftHand            8. RightHand
+  1. Yellow    2. Purple   3. Red
+  4. Orange    5. Blue     6. Green
+*/
 
 
 // Create a MIDI object
@@ -30,41 +32,60 @@ bool newState[] = {false, false, false, false, false, false, false, false};
 // Set initial key to C4 (Middle C)
 int key = 60;
 
+// Store major/minor status
+bool major = true;
+
 // Define notes to be played by each button - numbers represent how many semitones above tonic.
 int notes[] = {0, 2, 4, 5, 7, 9, 11, 12};
+
+
+void playChord(int tonic, bool isMajor) {
+  // Plays a chord for 1 second to indicate change of key or major/minor switch
+  // Stop all notes
+  // TODO: Find more efficient way to do this.
+  for (int i = 0; i < 128; i++) MIDI.sendNoteOff(i, 0, 1);
+  int chord[] = {-12, 0, isMajor ? 4 : 3, 7, 12};
+  for (int i : chord) {
+    int note = key + i;
+    MIDI.sendNoteOn(note, 127, 1);
+  }
+  delay(1000);
+  for (int i : chord) {
+    int note = key + i;
+    MIDI.sendNoteOff(note, 0, 1);
+  }
+}
+
 
 void keyChange(int newKey) {
   // Check if key within valid range
   if (newKey < 12 || newKey > 115) return;
   // Store the new key
   key = newKey;
-  // Stop all keys
-  for (int i = 0; i < 128; i++) MIDI.sendNoteOff(i, 0, 1);
   // Play major chord for 1 second to indicate success
-  int majorChord[] = {-12, 0, 4, 7, 12};
-  for (int i : majorChord) {
-    int note = key + i;
-    MIDI.sendNoteOn(note, 127, 1);
+  playChord(key, major);
+}
+
+void scaleChange() {
+  // Toggle between major and minor scales
+  major = !major;
+  if (major) {
+    notes[2] = 4;
+    notes[5] = 9;
+    notes[6] = 11;
+  } else {
+    notes[2] = 3;
+    notes[5] = 8;
+    notes[6] = 10;
   }
-  delay(1000);
-  for (int i : majorChord) {
-    int note = key + i;
-    MIDI.sendNoteOff(note, 0, 1);
-  }
+  playChord(key, major);  
 }
 
 void setup() {
   Serial.begin(19200);
 
   // Initialize the pushbutton pins as input
-  pinMode(buttonPinY, INPUT_PULLUP);
-  pinMode(buttonPinP, INPUT_PULLUP);
-  pinMode(buttonPinR, INPUT_PULLUP);
-  pinMode(buttonPinO, INPUT_PULLUP);
-  pinMode(buttonPinB, INPUT_PULLUP);
-  pinMode(buttonPinG, INPUT_PULLUP);
-  pinMode(handR, INPUT_PULLUP);
-  pinMode(handL, INPUT_PULLUP);
+  for (int pin : pins) pinMode(pin, INPUT_PULLUP);
 
   // Start MIDI communication
   MIDI.begin(MIDI_CHANNEL_OMNI);
@@ -72,31 +93,28 @@ void setup() {
 
 void loop() {
   // Read the state of the pushbutton values
-  for (int i = 0; i < 8; i++) {
-    if (i < 6) {
-      newState[i] = digitalRead(pins[i]) == LOW;
-    } else {
-      newState[i] = analogRead(pins[i]) < 900;
-    }
-  }
+  for (int i = 0; i < 8; i++) newState[i] = digitalRead(pins[i]) == LOW;
 
-  // Check for key change button presses
+  // Check for key/mode change button presses
   // Top three buttons - raise key
   if (newState[0] && newState[1] && newState[2]) keyChange(key + 1);
   // Bottom three buttons - lower key
   else if (newState[3] && newState[4] && newState[5]) keyChange(key - 1);
+  // Four buttons - toggle between major/minor
+  else if (newState[0] && newState[2] && newState[3] && newState[5]) scaleChange();
 
   // Action button presses
-    for (int i = 0; i < 8; i++) {
-      // No action required if button state unchanged
-      if (oldState[i] == newState[i]) continue;
-      // Update oldState with changed value
-      oldState[i] = newState[i];
-      int note = key + notes[i];
-      // Play note if pressed
-      if (newState[i]) MIDI.sendNoteOn(note, 127, 1);
-      // End note on release
-      else MIDI.sendNoteOff(note, 0, 1); 
-    }
-    delay(10);
+  for (int i = 0; i < 8; i++) {
+    // No action required if button state unchanged
+    if (oldState[i] == newState[i]) continue;
+    // Update oldState with changed value
+    oldState[i] = newState[i];
+    int note = key + notes[i];
+    // Play note if pressed
+    if (newState[i]) MIDI.sendNoteOn(note, 127, 1);
+    // End note on release
+    else MIDI.sendNoteOff(note, 0, 1); 
+  }
+  // Give board a rest - is this needed?
+  delay(20);
 }
